8e776c1f6aa3d4d17ca28d62ac9b9626cddddc2f
Jensone <hello@jensone.com>
2023-11-17 01:16:31 +0100

    🔧 corriger(package.json) : ajouter la dépendance "openai" à la version 4.19.0 ✨ fonctionnalité(Chat.jsx) : ajouter la fonctionnalité de chatbot en utilisant l'API OpenAI pour répondre aux questions de l'utilisateur

--- src/pages/Chat/Chat.jsx
+++ src/pages/Chat/Chat.jsx
@@ -1,25 +1,63 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useLocation, useNavigate } from 'react-router-dom'
 import confetti from 'canvas-confetti'
+import { process } from 'dotenv'
+import load from '../../assets/load.gif'
+
+// Chargement du package OpenAI
+import OpenAI from 'openai'
 
 function Chat() {
     const location = useLocation()
     const navigate = useNavigate()
     const formData = location.state?.formData
 
+    // Configuration de l'API OpenAI
+    const openai = new OpenAI({
+        dangerouslyAllowBrowser: true,
+        apiKey: import.meta.env.VITE_OPENAI_API_KEY, // Clé API depuis le fichier .env.local
+    })
+    const prePrompt = `Agis comme un chatbot qui répond à aux questions de ${formData.name}. 
+                    Répond en commençant par son prénom et une salutation originale. La thématique est ${formData.subject}. Voici sa question : `
+    const [promt, setPromt] = useState('') // Constante pour le prompt à envoyer à l'API
+    const [apiResponse, setApiResponse] = useState('') // Constante pour la réponse de l'API
+    const [loading, setLoading] = useState(false) // Constante pour le chargement en cours
+
+    // Fonction pour envoyer le prompt à l'API
+    const handleSubmit = async e => {
+        // Fonction asynchrone car on utilise await
+        e.preventDefault() // Empêche le rechargement de la page
+        setLoading(true) // On met le chargement à true
+        try {
+            const result = await openai.chat.completions.create({
+                model: 'gpt-3.5-turbo-0613', // Modèle à utiliser
+                messages: [{ role: 'user', content: prePrompt + promt }], // Prompt à envoyer à l'API
+            })
+            console.log(result.choices[0].message.content) // On affiche la réponse dans la console pour le débug
+            setApiResponse(result.choices[0].message.content) // On met la réponse de l'API dans la constante
+        } catch (error) {
+            setApiResponse('Une erreur est survenue') // On informe l'utilisateur d'une erreur
+        }
+        setLoading(false) // On met le chargement à false
+    }
+
     useEffect(() => {
         if (formData.name && formData.subject) {
+            // Si on a bien le nom et le sujet
             confetti({
+                // On lance les confettis
                 particleCount: 100,
                 spread: 70,
                 origin: { y: 0.6 },
             })
         } else {
+            // Sinon on redirige vers la page d'accueil
             navigate('/')
         }
     }, [formData, navigate])
 
     if (!formData) {
+        // Si on a pas de données, on redirige vers la page d'accueil
         return null
     }
 
@@ -29,15 +67,30 @@ function Chat() {
                 Bienvenue, {formData.name}
             </h1>
             <h3>Pose-moi tes questions sur {formData.subject}</h3>
-            <form>
+            <form onSubmit={handleSubmit}>
                 <div className="form-floating mb-3">
                     <textarea
                         className="form-control"
-                        placeholder="Pose ta question ici"    
+                        placeholder="Pose ta question ici"
+                        value={promt}
+                        onChange={e => setPromt(e.target.value)}
                     ></textarea>
                 </div>
-                <button className='btn  btn-warning rounded-pill' type="submit">Envoyer</button>
+                <button
+                    className="btn btn-warning rounded-pill"
+                    type="submit"
+                    disabled={loading || promt.length === 0}
+                >
+                    Envoyer
+                </button>
             </form>
+            <div className='mt-3'>
+                {loading && <img src={load} alt="Chargement" width={50} />}
+                <br />
+                <code className="text-left text-dark col-md-6 col-sm-10 m-auto p-3 my-3">
+                    {apiResponse}
+                </code>
+            </div>
         </main>
     )
 }
